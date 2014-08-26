fd73737d15c974b082667f00c0951c604e3fece0
Javier Salazar <javierslzrh@gmail.com>
2014-08-25 21:32:51 -0430

    Updated the files permissions.

--- src/config/config.php
+++ src/config/config.php
@@ -1,9 +1,7 @@
 <?php
 
 	return array(
-		'url' => 'http://10.10.1.89/smscaribe/index.php?app=ws',
-		'username' => 'admin',
-		'token' => '24eb4d7b8badb4291e3e448876674918'
+		'url' => '',
+		'username' => '',
+		'token' => ''
 	);
-
-?>
