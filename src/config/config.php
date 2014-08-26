8d5c36a6432187074acd5b0a94def5733b82ed16
Javier Salazar <javierslzrh@gmail.com>
2014-08-25 21:47:38 -0430

    Updated config docs.

--- src/config/config.php
+++ src/config/config.php
@@ -1,7 +1,18 @@
 <?php
 
 	return array(
+		/**
+		* URL - PlaySMS Webservices URL
+		*/
 		'url' => '',
+
+		/**
+		* USERNAME - PlaySMS username #
+		*/
 		'username' => '',
+
+		/**
+		* TOKEN - PlaySMS Webservices token
+		*/
 		'token' => ''
 	);
